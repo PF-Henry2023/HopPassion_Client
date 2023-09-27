3f4fd4ac62acb5f6c21332d37ff53f4927666c7c
Cintia Coccia <cintia_08@hotmail.com>
2023-09-27 12:56:09 -0300

    install vite + bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import LogoColor from "./LogoColor.png"
+
+function App() {
+
+  return (
+    <>
+      <h1>HOP PASSION</h1>
+      <img src={LogoColor} alt="Logo de color" width="100px" height="100px" />
+       
+    </>
+  )
+}
+
+export default App
