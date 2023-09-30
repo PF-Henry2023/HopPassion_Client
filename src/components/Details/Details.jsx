5b7b0279644243507fba3256bc1a8c7934b16ba9
MBGonzalezMiramontes <begonzalezmiramontes@gmail.com>
2023-09-30 13:31:14 -0300

    details basic loader

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../Navbar/Navbar";
 import styles from "./Details.module.css";
 import { useParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 import { getProductById } from "../../redux/actions/actions";
 
 import Footer from "../Footer/Footer";
@@ -12,10 +11,12 @@ const Details = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
   const productDetails = useSelector((state) => state.productDetails);
 
   useEffect(() => {
-    dispatch(getProductById(id));
+    setIsLoading(true);
+    dispatch(getProductById(id)).then(() => setIsLoading(false));
   }, [dispatch, id]);
 
   const handleGoBack = () => {
@@ -26,62 +27,66 @@ const Details = () => {
     <div>
       <div className={styles.mainContainer}>
         <Navbar />
-        <div className={styles.container}>
-          <div className={styles.column}>
-            <button className={styles.goBackButton} onClick={handleGoBack}>
-              Volver
-            </button>
-          </div>
-
-          <div className={styles.column}>
-            <div className={styles.imageContainer}>
-              <img
-                src={productDetails.image}
-                className={styles.image}
-                alt={`${productDetails.id}`}
-              />
+        {isLoading ? (
+          <p>Cargando...</p> // Muestra un mensaje de carga mientras isLoading sea true
+        ) : (
+          <div className={styles.container}>
+            <div className={styles.column}>
+              <button className={styles.goBackButton} onClick={handleGoBack}>
+                Volver
+              </button>
             </div>
-            <p className={styles.description}>{productDetails.description}</p>
-          </div>
 
-          <div className={styles.column}>
-            <h1 className={styles.title}>{productDetails.name}</h1>
-            <p className={styles.price}>$ {productDetails.price}</p>
-            <p className={styles.quantity}>Cantidad: </p>
-            <p className={styles.quantity}>
-              {productDetails.stock} unidades disponibles
-            </p>
-            <button className={styles.addToCartButton}>
-              + Agregar al carrito
-            </button>
-            <div className={styles.box}>
-              <div className={styles.firstrow}>
-                <div className={styles.row}>
-                  <h6 className={styles.subTitle}>Categoría:</h6>
-                  <p className={styles.subTitle}>American Ipa</p>
+            <div className={styles.column}>
+              <div className={styles.imageContainer}>
+                <img
+                  src={productDetails.image}
+                  className={styles.image}
+                  alt={`${productDetails.id}`}
+                />
+              </div>
+              <p className={styles.description}>{productDetails.description}</p>
+            </div>
+
+            <div className={styles.column}>
+              <h1 className={styles.title}>{productDetails.name}</h1>
+              <p className={styles.price}>$ {productDetails.price}</p>
+              <p className={styles.quantity}>Cantidad: </p>
+              <p className={styles.quantity}>
+                {productDetails.stock} unidades disponibles
+              </p>
+              <button className={styles.addToCartButton}>
+                + Agregar al carrito
+              </button>
+              <div className={styles.box}>
+                <div className={styles.firstrow}>
+                  <div className={styles.row}>
+                    <h6 className={styles.subTitle}>Categoría:</h6>
+                    <p className={styles.subTitle}>American Ipa</p>
+                  </div>
+                  <div className={styles.row}>
+                    <h6 className={styles.subTitle}>País de Origen:</h6>
+                    <p className={styles.subTitle}>{productDetails.country}</p>
+                  </div>
                 </div>
-                <div className={styles.row}>
-                  <h6 className={styles.subTitle}>País de Origen:</h6>
-                  <p className={styles.subTitle}>{productDetails.country}</p>
+                <div className={styles.secondRow}>
+                  <div className={styles.row}>
+                    <h6 className={styles.subTitle}>Graduación alcohólica:</h6>
+                    <p className={styles.subTitle}>
+                      {productDetails.alcoholContent}%
+                    </p>
+                  </div>
+                  <div className={styles.row}>
+                    <h6 className={styles.subTitle}>Volumen:</h6>
+                    <p className={styles.subTitle}>
+                      {productDetails.amountMl} ml.
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div className={styles.secondRow}>
-                <div className={styles.row}>
-                  <h6 className={styles.subTitle}>Graduación alcohólica:</h6>
-                  <p className={styles.subTitle}>
-                    {productDetails.alcoholContent}%
-                  </p>
-                </div>
-                <div className={styles.row}>
-                  <h6 className={styles.subTitle}>Volumen:</h6>
-                  <p className={styles.subTitle}>
-                    {productDetails.amountMl} ml.
-                  </p>
-                </div>
-              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
       <Footer />
     </div>
