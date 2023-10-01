3be185255120bba295aa6c6475cf7aafbf9d9fa6
Henry Corredor González <henry.corredor7570@gmail.com>
2023-09-30 21:38:45 -0500

    pagination

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import style from "../CardContainer/CardContainer.module.css"
 import {useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../redux/actions/actions";
-import Card from "../Card/Card"
+import Card from "../Card/Card";
+import Pagination from "../Pagination/Pagination";
 
 export default function CardContainer () {
     
@@ -13,6 +14,17 @@ export default function CardContainer () {
         dispatch(getProducts())
     }, [])
 
+    const [currentPage, setCurrentPage] = useState(1);
+    const cervezasPerPage = 12;
+    
+    const indexLast = currentPage * cervezasPerPage; //calcula el indice del ultimo pokemon.
+    const indexFirst = indexLast - cervezasPerPage; //calcula el indice del primer pokemon.
+    const currentCervezas = products ? products.slice(indexFirst, indexLast) : [];
+
+    const paginado = (pageNumber) => {// funcion para cambiar la pagina actual.
+        setCurrentPage(pageNumber);
+    }
+
     
     return (
         <div className={style.container}>
@@ -20,11 +32,19 @@ export default function CardContainer () {
             <div className={style.subtitle}>Selección de las mejores cervezas</div>
             <div className={style.gridContainer}>
             {
-                products == null ? <></> : products.map((product) => {
+                currentCervezas == null ? <></> : currentCervezas.map((product) => {
                     return <Card key={product.id} id={product.id} title={product.name} price={product.price} image={product.image}/>
                 })
             }
-             </div>
+            </div>
+            <div>
+                <Pagination
+                    currentPage={currentPage}
+                    cervezasPerPage={cervezasPerPage}
+                    allCervezas={products ? products.length : 0}
+                    paginado={paginado}
+                ></Pagination>
+            </div>
         </div>
     )
 }
