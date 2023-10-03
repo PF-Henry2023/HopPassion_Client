5b53f9e7926dab6057aa6afc7217a69c56b4e05e
spadonienzo <spadonienzo@gmail.com>
2023-10-03 15:51:27 -0300

    Dropdown paises y alert con sweet alert

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -97,7 +97,7 @@ export const createProduct = ({
   amountMl,
   alcoholContent,
 }) => {
-  console.log(image);
+  
   return async function (dispatch) {
     try {
       const response = await axios.post(
@@ -114,8 +114,6 @@ export const createProduct = ({
           alcoholContent,
         }
       );
-      console.log(response);
-      alert("Producto creado exitosamente");
       return dispatch({
         type: CREATE_PRODUCT,
       });
