eaf1f6a33b3206aa5182dc4bd46eb1f7903764ff
MBGonzalezMiramontes <begonzalezmiramontes@gmail.com>
2023-10-06 09:58:47 -0300

    cart style

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,6 +8,8 @@ import {
 import styles from "./Cart.module.css";
 import QuantityControl from "../QuantityControl/QuantityControl";
 import Return from "../Return/Return";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 const Cart = () => {
   const dispatch = useDispatch();
@@ -30,39 +32,40 @@ const Cart = () => {
   };
 
   return (
-    <div className={styles.cartContainer}>
-      {/* Columna 1 */}
-      <div className={styles.column}>
-        <Return />
-      </div>
-
-      {/* Columna 2 */}
-      <div className={styles.column}>
-        <div className={styles.cartTotal}>
-          <h1>Mi carrito</h1>
+    <>
+      <Navbar />
+      <div className={styles.cartContainer}>
+        {/* Columna 1 */}
+        <div className={styles.column}>
+          <Return />
         </div>
 
-        {/* Subtítulos en la segunda fila */}
-        <div className={styles.subtitles}>
-          <div className={styles.subtitle}>Producto</div>
-          <div className={styles.subtitle}>Precio</div>
-          <div className={styles.subtitle}>Cantidad</div>
-          <div className={styles.subtitle}></div>{" "}
-          {/* Espacio para el botón de eliminar */}
-        </div>
+        {/* Columna 2 */}
+        <div className={styles.column}>
+          <div className={styles.cartTotal}>
+            <h1>Mi carrito</h1>
+          </div>
 
-        {/* Lista de productos */}
-        {cart.map((cartItem) => (
-          <div key={cartItem.id} className={styles.cartItem}>
-            <div className={styles.productInfo}>
-              <img
-                src={cartItem.image}
-                className={styles.cartItemImage}
-                alt={`${cartItem.id}`}
-              />
-              <div className={styles.cartItemName}>{cartItem.title}</div>
-            </div>
-            <div className={styles.cartItemDetails}>
+          {/* Subtítulos en la segunda fila */}
+          <div className={styles.subtitles}>
+            <div className={styles.subtitle}>Producto</div>
+            <div className={styles.subtitle}>Precio</div>
+            <div className={styles.subtitle}>Cantidad</div>
+            <div className={styles.subtitle}></div>{" "}
+            {/* Espacio para el botón de eliminar */}
+          </div>
+
+          {/* Lista de productos */}
+          {cart.map((cartItem) => (
+            <div key={cartItem.id} className={styles.cartItem}>
+              <div>
+                <img
+                  src={cartItem.image}
+                  className={styles.cartItemImage}
+                  alt={`${cartItem.id}`}
+                />
+                <div className={styles.cartItemName}>{cartItem.title}</div>
+              </div>
               <p className={styles.cartItemPrice}>
                 Precio por unidad: ${cartItem.price.toFixed(2)}
               </p>
@@ -74,8 +77,6 @@ const Cart = () => {
                   handleQuantityChange(cartItem.id, newQuantity)
                 }
               />
-            </div>
-            <div className={styles.cartItemButtons}>
               <button
                 className={styles.cartItemButton}
                 onClick={() => handleRemoveFromCart(cartItem.id)}
@@ -83,47 +84,51 @@ const Cart = () => {
                 Eliminar
               </button>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
 
-      {/* Columna 3 */}
-      <div className={styles.column}>
-        <div className={styles.subtotal}>
-          <div>Subtotal</div>
-          <div>
-            $
-            {cart
-              .reduce((total, item) => total + calculateCartItemTotal(item), 0)
-              .toFixed(2)}
+        {/* Columna 3 */}
+        <div className={styles.column}>
+          <div className={styles.subtotal}>
+            <div>Subtotal</div>
+            <div>
+              $
+              {cart
+                .reduce(
+                  (total, item) => total + calculateCartItemTotal(item),
+                  0
+                )
+                .toFixed(2)}
+            </div>
           </div>
-        </div>
-        <div className={styles.divider}></div>
-        <div className={styles.gastosEnvio}>
-          <div>Gastos de envío</div>
-          <div>$500</div>
-        </div>
-        <div className={styles.divider}></div>
-        <div className={styles.total}>
-          <div>Total</div>
-          <div>
-            $
-            {(
-              cart.reduce(
-                (total, item) => total + calculateCartItemTotal(item),
-                0
-              ) + 500
-            ) // Agrega los gastos de envío al total
-              .toFixed(2)}
+          <div className={styles.divider}></div>
+          <div className={styles.gastosEnvio}>
+            <div>Gastos de envío</div>
+            <div>$500</div>
           </div>
-        </div>
-        <div className={styles.buttons}>
-          <button>Proceder al pago</button>
-          <button>Elegir más productos</button>
-          <button onClick={handleClearCart}>Vaciar Carrito</button>
+          <div className={styles.divider}></div>
+          <div className={styles.total}>
+            <div>Total</div>
+            <div>
+              $
+              {(
+                cart.reduce(
+                  (total, item) => total + calculateCartItemTotal(item),
+                  0
+                ) + 500
+              ) // Agrega los gastos de envío al total
+                .toFixed(2)}
+            </div>
+          </div>
+          <div className={styles.buttons}>
+            <button>Proceder al pago</button>
+            <button>Elegir más productos</button>
+            <button onClick={handleClearCart}>Vaciar Carrito</button>
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
