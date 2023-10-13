66edac95ac58c9f01cffe6320d0bd03ae123eb23
OfirRoque <gabrielroque803@gmail.com>
2023-10-13 13:15:40 -0500

    dev89

---
+++ src/components/Stadistics/Area_Chart/Areachart.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import axios from "axios";
+
+function AreaChart() {
+  const [ info, setInfo ] = useState([]);
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend
+  );
+  
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  };
+  
+  const labels = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/stadistics/monthlyIncomeForTheYear?type=amount")
+      .then((response) => {
+        setInfo(response.data.data)
+      })
+      .catch((error) => console.log(error))
+  },[])
+  
+  const data = {
+    labels,
+    datasets: [
+      {
+        fill: true,
+        label: "Cantidad de ventas",
+        data: info,
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+  return <Line options={options} data={data} />;
+}
+
+export default AreaChart;
