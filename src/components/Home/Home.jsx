cc430b9ad7043bea3c7f64ee84f09449a6d3f81f
MBGonzalezMiramontes <begonzalezmiramontes@gmail.com>
2023-10-13 03:11:53 -0300

    carousel done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,11 +12,13 @@ import logotype from "../../assets/logo_brand.png";
 import Carousel from "react-bootstrap/Carousel";
 import { Link, useNavigate } from "react-router-dom";
 import style from "./Home.module.css";
+import { getLoggedInUser } from "../../utils/UserUtils";
 
 const Home = () => {
   const navigate = useNavigate();
   const refAllProducts = useRef(null);
   const refFeaturedProducts = useRef(null);
+  const user = getLoggedInUser();
 
   const handleNavigate = () => {
     navigate("/signup");
@@ -33,77 +35,136 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      {/* <ControlledCarousel  /> */}
-      <Carousel>
-        <Carousel.Item>
-          <div className={style.imageContainer}>
-            <img
-              className={`d-block w-100 ${style.image}`}
-              src={image1}
-              alt="First slide"
-            />
-            <div className={style.overlay}>
-              <img src={logotype} alt="Logo" className={style.logo} />
-              <p className={style.description}>
-                Tu destino en línea para explorar y adquirir una amplia variedad
-                de cervezas artesanales y especialidades excepcionales.
-              </p>
-              <hr />
-              <button
-                onClick={handleScrollToAllProducts}
-                className={style.button}
-              >
-                Descubre Nuestras Cervezas
-              </button>
-            </div>
-          </div>
-        </Carousel.Item>
 
-        <Carousel.Item>
-          <div className={style.imageContainer}>
-            <img
-              className={`d-block w-100 ${style.image}`}
-              src={image2}
-              alt="First slide"
-            />
-            <div className={style.overlay}>
-              <img src={logotype} alt="Logo" className={style.logo} />
-              <p className={style.description}>
-                ¡Únete a nuestra comunidad cervecera
-              </p>
-              <p className={style.description}>
-                y descubre un mundo de sabores!
-              </p>
-              <hr />
-              <button onClick={handleNavigate} className={style.button}>
-                Registrate
-              </button>
-              <Link className={style.link} to="/login">
-                {" "}
-                <h5>o Inicia seión</h5>
-              </Link>
+      {user ? (
+        <Carousel>
+          <Carousel.Item>
+            <div className={style.imageContainer}>
+              <img
+                className={`d-block w-100 ${style.image}`}
+                src={image1}
+                alt="First slide"
+              />
+              <div className={style.overlay}>
+                <img src={logotype} alt="Logo" className={style.logo} />
+                <p className={style.description}>
+                  Tu destino en línea para explorar y adquirir una amplia
+                  variedad de cervezas artesanales y especialidades
+                  excepcionales.
+                </p>
+                <hr />
+                <button
+                  onClick={handleScrollToAllProducts}
+                  className={style.button}
+                >
+                  Descubre Nuestras Cervezas
+                </button>
+              </div>
             </div>
-          </div>
-        </Carousel.Item>
+          </Carousel.Item>
 
-        <Carousel.Item>
-          <div className={style.imageContainer}>
-            <img
-              className={`d-block w-100 ${style.image}`}
-              src={image3}
-              alt="First slide"
-            />
-            <div className={style.overlay}>
-              <img src={logotype} alt="Logo" className={style.logo} />
-              <p className={style.description}>
-                Descubre Nuestras Cervezas Destacadas
-              </p>
-              <hr />
-              <button className={style.button}>Explorar Ahora</button>
+          <Carousel.Item>
+            <div className={style.imageContainer}>
+              <img
+                className={`d-block w-100 ${style.image}`}
+                src={image3}
+                alt="First slide"
+              />
+              <div className={style.overlay}>
+                <img src={logotype} alt="Logo" className={style.logo} />
+                <p className={style.description}>
+                  Descubre Nuestras Cervezas Destacadas
+                </p>
+                <hr />
+                <button
+                  className={style.button}
+                  onClick={handleScrollToFeaturedProducts}
+                >
+                  Explorar Ahora
+                </button>
+              </div>
             </div>
-          </div>
-        </Carousel.Item>
-      </Carousel>
+          </Carousel.Item>
+        </Carousel>
+      ) : (
+        <Carousel>
+          <Carousel.Item>
+            <div className={style.imageContainer}>
+              <img
+                className={`d-block w-100 ${style.image}`}
+                src={image1}
+                alt="First slide"
+              />
+              <div className={style.overlay}>
+                <img src={logotype} alt="Logo" className={style.logo} />
+                <p className={style.description}>
+                  Tu destino en línea para explorar y adquirir una amplia
+                  variedad de cervezas artesanales y especialidades
+                  excepcionales.
+                </p>
+                <hr />
+                <button
+                  onClick={handleScrollToAllProducts}
+                  className={style.button}
+                >
+                  Descubre Nuestras Cervezas
+                </button>
+              </div>
+            </div>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <div className={style.imageContainer}>
+              <img
+                className={`d-block w-100 ${style.image}`}
+                src={image3}
+                alt="First slide"
+              />
+              <div className={style.overlay}>
+                <img src={logotype} alt="Logo" className={style.logo} />
+                <p className={style.description}>
+                  Descubre Nuestras Cervezas Destacadas
+                </p>
+                <hr />
+                <button
+                  className={style.button}
+                  onClick={handleScrollToFeaturedProducts}
+                >
+                  Explorar Ahora
+                </button>
+              </div>
+            </div>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <div className={style.imageContainer}>
+              <img
+                className={`d-block w-100 ${style.image}`}
+                src={image2}
+                alt="First slide"
+              />
+              <div className={style.overlay}>
+                <img src={logotype} alt="Logo" className={style.logo} />
+                <p className={style.description}>
+                  ¡Únete a nuestra comunidad cervecera
+                </p>
+                <p className={style.description}>
+                  y descubre un mundo de sabores!
+                </p>
+                <hr />
+                <button onClick={handleNavigate} className={style.button}>
+                  Registrate
+                </button>
+                <Link className={style.link} to="/login">
+                  {" "}
+                  <h5>o Inicia seión</h5>
+                </Link>
+              </div>
+            </div>
+          </Carousel.Item>
+        </Carousel>
+      )}
+      
       <div className={style.featuredProductsContainer}>
         <div className={style.marginOne}>
           <h1 className={style.titleFeaturedProducts} ref={refFeaturedProducts}>
