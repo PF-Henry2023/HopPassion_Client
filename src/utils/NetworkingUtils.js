1347d46ba636f8dea2f526331acbadfe0c7ab707
MBGonzalezMiramontes <begonzalezmiramontes@gmail.com>
2023-10-18 11:50:33 -0300

    url

--- src/utils/NetworkingUtils.js
+++ src/utils/NetworkingUtils.js
@@ -4,7 +4,8 @@ import store from "../redux/store/store";
 import { navigate } from "./NavigationUtils";
 
 const HopPassionClient = axios.create({
-  baseURL: "http://hoppassion-server.1.ie-1.fl0.io/",
+  baseURL: "https://hoppassion-server.1.ie-1.fl0.io/",
+  // baseURL: "http://hoppassion-server.1.ie-1.fl0.io/",
 });
 
 //otra forma mas corta seria:
