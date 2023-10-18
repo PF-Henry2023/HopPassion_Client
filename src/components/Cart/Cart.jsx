0a774fcd8287aeda91a75fdec6f7740d981a2091
Cintia Coccia <cintia_08@hotmail.com>
2023-10-18 01:41:48 -0300

    cart row

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
-  removeFromCart,
   getCart,
   getCartRequest,
-  addToCart,
 } from "../../redux/actions/actions";
 import styles from "./Cart.module.css";
 import Return from "../Return/Return";
@@ -12,61 +10,20 @@ import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import Loading from "../Loading/Loading";
 import MercadoPagoComponent from "./MercadoPagoButtom/Buttom";
-import TimedCounter from "../Counter/TimedCounter";
-import { Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import CartRow from "./CartRow";
 
 const Cart = () => {
   const dispatch = useDispatch();
-  const [isDeleting, setDeleting] = useState([]);
   const syncing = useSelector((state) => state.cart.syncing);
   const cart = useSelector((state) => state.cart);
   const navigate = useNavigate();
-  const [ updating, setUpdating ] = useState({})
 
   useEffect(() => {
     dispatch(getCartRequest());
     dispatch(getCart());
   }, []);
-
-  const handleRemoveFromCart = (productId) => {
-    const deleting = [...isDeleting];
-    deleting.push(productId);
-    setDeleting(deleting);
-    const productToRemove = cart.products.find(
-      (product) => product.id === productId
-    );
-    if (!productToRemove) {
-      alert("El producto no se encuentra en el carrito");
-      return;
-    }
-    dispatch(removeFromCart(productId, (result) => {}));
-  };
-
-  const isLoading = (productId) => {
-    return isDeleting.includes(productId) || syncing;
-  };
-
-  function deleteButtonContent(productId) {
-    if (isLoading(productId)) {
-      return <Spinner animation="border" role="status"></Spinner>;
-    } else {
-      return "Eliminar";
-    }
-  }
-
-  function handleQuantityChange(productId, newQuantity) {
-    const u = { ...updating }
-    u[productId] = true
-    setUpdating(u)
-    dispatch(addToCart(productId, newQuantity, (result) => {
-      const u = { ...updating }
-      u[productId] = false
-      setUpdating(u)
-    }))
-    console.log(`id: ${productId}, new quantity: ${newQuantity}`)
-  }
 
   const handleNavigate = () => {
     navigate("/");
@@ -99,37 +56,7 @@ const Cart = () => {
 
             {/* Lista de productos */}
             {(cart.products ?? []).map((product) => (
-              <div key={product.id} className={styles.cartItem}>
-                <div className={styles.productGroup}>
-                  <img
-                    src={product.image}
-                    className={styles.cartItemImage}
-                    alt={`${product.id}`}
-                  />
-                  <div className={styles.cartItemName}>{product.name}</div>
-                </div>
-                <p className={styles.cartItemPrice}>$ {product.price}</p>
-                {/* <Counter
-                productId={product.id}
-                initialQuantity={product.quantity}
-                stock={product.stock}
-                onQuantityChange={(newQuantity) => {}}
-              /> */}
-                {/* cart.quantities[product.id] */}
-                <TimedCounter
-                initialQuantity={cart.quantities[product.id]}
-                productId={product.id}
-                stock={product.stock}
-                loading={updating[product.id]}
-                onQuantityChange={(nq) => handleQuantityChange(product.id, nq)}
-                />
-                <button
-                  className={styles.cartItemButton}
-                  onClick={() => handleRemoveFromCart(product.id)}
-                >
-                  {deleteButtonContent(product.id)}
-                </button>
-              </div>
+              <CartRow key={product.id} initialQuantity={cart.quantities[product.id]} product={product} syncing={syncing}/>
             ))}
           </div>
 
@@ -142,12 +69,6 @@ const Cart = () => {
               </div>
               <div className={styles.divider}></div>
 
-              {/* <div className={styles.gastosEnvio}>
-            <div>Gastos de envío</div>
-            <div>$500</div>
-          </div>
-          <div className={styles.divider}></div>*/}
-
               <div className={styles.total}>
                 <div>Total</div>
                 <div>$ {cart.total}</div>
@@ -159,7 +80,6 @@ const Cart = () => {
                 <button onClick={handleNavigate} className={styles.btn}>
                   Elegir más productos
                 </button>
-                {/* <button onClick={handleClearCart}>Vaciar Carrito</button> */}
               </div>
             </div>
           </div>
