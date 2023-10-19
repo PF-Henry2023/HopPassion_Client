f6a39d40ae05e971f25c5f3d23022520beb7debd
MBGonzalezMiramontes <begonzalezmiramontes@gmail.com>
2023-10-18 22:50:04 -0300

    merge problems

--- src/utils/NetworkingUtils.js
+++ src/utils/NetworkingUtils.js
@@ -4,15 +4,7 @@ import store from "../redux/store/store";
 import { navigate } from "./NavigationUtils";
 
 const HopPassionClient = axios.create({
-<<<<<<< HEAD
-<<<<<<< HEAD
-  baseURL: "http://hoppassion-server.1.ie-1.fl0.io/",
-=======
   baseURL: "https://hoppassion-server.1.ie-1.fl0.io",
->>>>>>> d24423aa189c50113bdfd5cf703536277373dba9
-=======
-  baseURL: "https://hoppassion-server.1.ie-1.fl0.io",
->>>>>>> userProfile
 });
 
 HopPassionClient.interceptors.request.use(
